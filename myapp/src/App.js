7ec5c1b9174e9ad8e24f6bf1912692dd81486b3f
kimrachelponchan <jgim9182@gmail.com>
2025-11-20 22:04:09 +0900

    서버 연결하기

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,66 +1,42 @@
 import './App.css';
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const boxRef = useRef();
+  const [userInfo, setUserInfo] = useState({userID:"", userPassword:""});
 
-  const scrollToBottom = () => {
-    boxRef.current.scrollTo({
-      top: boxRef.current.scrollHeight, // 맨 아래
-      behavior: "smooth"                // 부드럽게 스크롤
-    });
-    console.log(boxRef);
-  };
+  useEffect(()=>{
+    console.log(userInfo);
+  },[userInfo]);
+
+
+  const onChange = (e)=>{
+    const { name, value } = e.target;
+
+    setUserInfo(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  const onSubmit = async ()=>{
+    await axios.post(`http://localhost:5000/`,[userInfo]);
+  }
+
 
   return (
-    <div>
-      <div
-        ref={boxRef}
-        style={{
-          height: "100px",
-          overflowY: "scroll",
-          border: "1px solid black"
-        }}
-      >
-        {/*Array.from(길이, 값) <- 문법이 이렇게 됌 */}
-        {/* Array.from({length: 30}) -> [udefind...50개] */}
-        
-        {Array.from({ length: 50 }, (_, i) => (
-          <p key={i}>Item {i + 1}</p>
-        ))}
-
+    <>
+      <div>
+        <label>아이디:</label>
+        <input onChange={onChange} name={`userID`} type={`text`}></input>
       </div>
-      <button
-        onClick={scrollToBottom}
-        className="mt-2 px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-400"
-      >
-        맨 아래로
-      </button>
-    </div>
+      <div>
+        <label>비밀번호:</label>
+        <input onChange={onChange} name={`userPassword`} type={`password`}></input>
+      </div>
+      <button onClick={onSubmit} className='button-style2'>전송하기</button>
+    </>
   );
 }
 
 export default App;
-
-
-// 파이썬, 자바, 자바스크립트 기본 클래스 문법
-
-// class Person:
-//           def __init__(self,name, age):
-//             self.name = name
-//             self.age = age
-
-//           def say(self):
-//             print(self.name);
-          
-//           def modify(self):
-//             self.name = ""
-//             self.age = ""
-
-//           def ageUp(self):
-//             self.age = self.age + 1
-
-//         a = Person("Tom",20)
-//         a.say()
-//         a.modify()
-//         a.ageUp()
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 
 
 // 중요한 파일 설정을 가져오기위해 NodeJS에서 env파일을 읽을 수 있도록 설정하는 코드
@@ -10,8 +10,20 @@ dotenv.config({path: "./config/.env"});
 const app = express();
 const PORT = process.env.SERVER_PORT | 5000;
 
-app.get('/', (req, res)=>{
-    res.send("안녕하세요");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+app.post('/', (req, res)=>{
+    console.log("클라이언트에서 요청됌!");
+    const userInfo = req.body;
+
+    console.log(userInfo);
+    res.send("요청성공!");
+})
+
+app.get('/', (req,res)=>{
+    res.send("요청성공!");
 })
 
 app.listen(PORT,()=>{
