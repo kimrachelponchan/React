87bae2a833e8c5e773b0629ff03b3f73463fc49d
kimrachelponchan <jgim9182@gmail.com>
2025-11-22 21:16:35 +0900

    로그인 화면 구현 중

--- myapp/src/login.js
+++ myapp/src/login.js
@@ -13,7 +13,7 @@ export const Login = () => {
         console.log(userInfo)
     },[userInfo])
 
-    
+
     function handleInput(e){
         const {name, value} = e.target;
         setUseinfo((prev) => ({...prev, [name] : value }));
@@ -22,20 +22,15 @@ export const Login = () => {
     }
 
 
-    async function handleSubmit (){
-        const result = await axios.post('http://localhost:5000/api/login', userInfo);
-        if(result){
-            console.log(result.data);
-        }
-    }
-
 
     return(
         <div className="w-[100vw] h-[100vh] bg-gray-100">
-            <div className="flex justify-center">Login</div>
-            <div className="flex"><p className="mr-4">아이디</p><input name={`id`} onChange={handleInput} type="text" placeholder="아이디 입력"/></div>
-            <div className="flex"><p className="mr-4">비밀번호</p><input name={`password`} onChange={handleInput} type="password" placeholder="비밀번호 입력"/></div>
-            <button onClick={handleSubmit}  className="bg-blue-300 rounded-xl px-5 py-1 font-bold" type="submit">전송</button>
+            <div>
+                <div className="flex justify-center">Login</div>
+                <div className="flex"><p className="mr-4">아이디</p><input name={`id`} onChange={handleInput} type="text" placeholder="아이디 입력"/></div>
+                <div className="flex"><p className="mr-4">비밀번호</p><input name={`password`} onChange={handleInput} type="password" placeholder="비밀번호 입력"/></div>
+                <button className="bg-blue-300 rounded-xl px-5 py-1 font-bold" type="submit">로그인</button>
+            </div>
         </div>
     )
 
