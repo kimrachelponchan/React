d8fc4af6be4b0a91d1fb5a341dc2b77c50a3fcf4
kimrachelponchan <jgim9182@gmail.com>
2025-11-23 01:39:18 +0900

    로그인 예외처리, 성공 후 페이지 이동

--- myapp/src/login.js
+++ myapp/src/login.js
@@ -1,37 +1,77 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
 
-    const [userInfo, setUseinfo] = useState({
-        id : '',
-        password : ''
-    })
+  const [userInfo, setUseinfo] = useState({
+    id: "",
+    password: "",
+  });
 
-    useEffect(()=>{
-        console.log(userInfo)
-    },[userInfo])
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
 
+  // 함수 선언식
+  function handleInput(e) {
+    const { name, value } = e.target;
+    setUseinfo((prev) => ({ ...prev, [name]: value }));
+    console.log(value);
+  }
 
-    function handleInput(e){
-        const {name, value} = e.target;
-        setUseinfo((prev) => ({...prev, [name] : value }));
-        console.log(value);
+  // 함수 표현식(변수에 할당)
+  const handleSubmit = async () => {
+    const result = await axios.post(
+      `http://localhost:5000/api/login`,
+      userInfo
+    );
 
+    // 예외처리
+    console.log(result.data);
+    if (result.data.id_check === false) {
+      alert(result.data.msg);
+    } else if (result.data.pw_check === false) {
+      alert(result.data.msg);
+    } else {
+      // 예외 처리를 통과하면 홈으로 이동
+      navigate("/home");
     }
+  };
 
-
-
-    return(
-        <div className="w-[100vw] h-[100vh] bg-gray-100">
-            <div>
-                <div className="flex justify-center">Login</div>
-                <div className="flex"><p className="mr-4">아이디</p><input name={`id`} onChange={handleInput} type="text" placeholder="아이디 입력"/></div>
-                <div className="flex"><p className="mr-4">비밀번호</p><input name={`password`} onChange={handleInput} type="password" placeholder="비밀번호 입력"/></div>
-                <button className="bg-blue-300 rounded-xl px-5 py-1 font-bold" type="submit">로그인</button>
-            </div>
+  return (
+    <div className="w-screen h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white w-[40%] flex flex-col p-5 rounded-2xl gap-6">
+        <h1 className="text-center bg-white">Login</h1>
+        <div className="flex bg-yellow-100">
+          <label className="mr-4">아이디</label>
+          <input
+            name={`id`}
+            onChange={handleInput}
+            type="text"
+            placeholder="아이디 입력"
+            className="border border-gray-300 rounded-lg"
+          />
         </div>
-    )
-
-}
+        <div className="flex bg-gray-100">
+          <label className="mr-4">비밀번호</label>
+          <input
+            name={`password`}
+            onChange={handleInput}
+            type="password"
+            placeholder="비밀번호 입력"
+            className="border border-gray-300 rounded-lg"
+          />
+        </div>
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-300 rounded-xl px-5 py-1 font-bold"
+          type="submit"
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
+};
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,13 +1,58 @@
 import db from "../config/db.js";
+import bcrypt from "bcrypt";
 
 // 사용자의 상호작용에 의한 로직 정리
-export const login = async (req, res)=>{
-    const { id, password } = req.body;
-    try{
-        const [row] = await db.query(`INSERT INTO users(user_id, user_password) VALUES(?,?)`,[id, password]);
-        return res.status(200).json({msg:"요청성공!", res: row});
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({massage:"요청실패!"});
+export const login = async (req, res) => {
+  const { id, password } = req.body;
+
+  // 나중에 회원가입 구현할 때 해야하는 로직
+  // 비밀번호 해시를 게산 반복횟수(숫자가 높을수록 느려짐 즉, 해킹시도가 어려워짐)
+  //   const saltRounds = 10;
+
+  // 비밀번호를 암호화하는 로직
+  // 해시가 되기도 전에 빈 값이 들어감 **await**
+  //   const secretPassword = await bcrypt.hash(password, saltRounds);
+
+  // 로그인 구현
+  // 아이디 검증 부분은 생력
+  // 비밀번호만 비교하고 로그인 처리 구햔
+
+  try {
+    // 현재 아이디를 입력한 사용자가 데이터베이스에 존재하는지 확인
+    // 사용자가 있으면 예) row = [{user_id: "abc", user_password: "hashedPW"}]
+    const [row] = await db.query(
+      `SELECT user_id, user_password FROM users WHERE user_id = ?`,
+      [id]
+    );
+
+    // const [row] = await db.query(
+    //   `INSERT INTO users(user_id, user_password) VALUES(?,?)`,
+    //   [id, secretPassword]
+    // );
+
+    // 해당 아이디가 존재하지 않으면 예외처리
+    if (row.length === 0) {
+      console.log("존재하지 않는 사용자입니다");
+      return res
+        .status(200)
+        .json({ msg: "존재하지 않는 사용자입니다.", id_check: false });
+    } else {
+      const isMatch = await bcrypt.compare(password, row[0].user_password);
+
+      if (isMatch) {
+        console.log("로그인 성공!");
+        return res
+          .status(200)
+          .json({ msg: "비밀번호가 일치합니다.", pw_check: true });
+      } else {
+        console.log("로그인 실패ㅜㅜ");
+        return res
+          .status(200)
+          .json({ msg: "비밀번호가 일치하지않습니다.", pw_check: false });
+      }
     }
-}
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ massage: "요청실패!" });
+  }
+};
