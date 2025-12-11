8ce52b7b2e6cfbb8e77f3f95dfd771af273d7c88
kimrachelponchan <jgim9182@gmail.com>
2025-12-11 17:27:51 +0900

    IntersectionObserver활용2

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -1,47 +1,13 @@
 import {useEffect, useState, useRef} from "react";
 
 export const Home = () => {
-
-
-    const contentsRef = useRef(null);
-    const [contents, setContents] = useState(false);
-
-    useEffect(()=>{ 
-        
-        const observer = new IntersectionObserver(
-            ([entry])=>{
-                if(entry.isIntersecting){
-                    console.log("요소가 보입니다.");
-                    setContents(true);
-                }else{
-                    setContents(false);
-                }
-            },{
-                threshold: 0.5
-            }
-        )
-
-        observer.observe(contentsRef.current);
-
-    },[])
-
-
-
     return(
         <>
             <Home.Layout>
                 <Home.Contents>
-                    <img src="image1.jpg"/>
-                    {/* <div ref={contentsRef} className="h-10 flex justify-center items-center">감시자</div> */}
-
-                    {/* 둘의 방식은 유사하지만 1번 요소의 경우에는 돔에 항상 존재하다가 class만 추가되는 형식이라 브라우저가 애니메이션으로 인식함 */}
-                    {/* 2번 요소의 경우에는 돔에 존재자체가 없다가 새로 생기고 없어지는 방식이기 때문에 class의 변경이 아닌 돔의 변경이라 애미메이션으로 인식하지 않음 */}
-                    <div className={`absolute w-full h-full flex justify-center items-center text-3xl duration-[1s] ${contents ? "opacity-100" : "opacity-0"}`}>요소가 노출됩니다.</div>
-                    {/* {contents && 
-                    (<div className={`absolute w-full h-full flex justify-center items-center text-3xl duration-[1s]`}>요소가 노출됩니다.</div>)
-                    } */}
-                    <img ref={contentsRef} src="image2.jpg"/>
-                    <img src="image3.jpg"/>
+                    <ImageContainer
+                        items={["image1.jpg","image2.jpg","image3.jpg"]}
+                    />
                 </Home.Contents>
             </Home.Layout>
         </>
@@ -56,5 +22,48 @@ Home.Layout = ({children})=>{
 }
 
 Home.Contents = ({children})=>{
-    return <div className="bg-white h-[400px] w-[600px] overflow-y-scroll layout relative cover">{children}</div>
+    return <div className="bg-white h-[400px] w-[600px]">{children}</div>
+}
+
+export const ImageContainer = ({items})=>{
+    const contentsRef = useRef(null);
+    const scrollRef = useRef(null);
+    const [contents, setContents] = useState(false);
+
+    useEffect(()=>{ 
+        
+        const observer = new IntersectionObserver(
+            ([entry])=>{
+                if(entry.isIntersecting){
+                    console.log("요소가 보입니다.");
+                    console.log(scrollRef.current.scrollTop);
+                    setContents(true);
+                }else if(scrollRef.current.scrollTop < 280){
+                    setContents(false);
+                }
+            },{
+                threshold: 0.7
+            }
+        )
+
+        observer.observe(contentsRef.current);
+
+    },[])
+
+    if(!items) return;
+
+    return (
+        <>
+            <div ref={scrollRef} className="w-full h-full overflow-y-scroll layout relative cover">
+                {items.map((item,index)=>{
+                    return(
+                        <>
+                            <img src={item} ref={index === 1 ? contentsRef : null} />
+                            {index === 0 &&<div className={`absolute w-full h-full flex justify-center items-center text-3xl duration-[1s] ${contents ? "left-show" : "right-out"}`}>요소가 노출됩니다.</div>}
+                        </>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
